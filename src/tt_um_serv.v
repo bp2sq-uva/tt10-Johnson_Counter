594abdd1379941a832afb4301a95083b8cc563c4
Kavish Ranawella <66011681+kavishranawella@users.noreply.github.com>
2025-05-02 02:35:00 -0400

    Evaluate SERV without SPI

--- src/tt_um_serv.v
+++ src/tt_um_serv.v
@@ -9,7 +9,7 @@
 
 `default_nettype none
 
-module tt_um_spi_serv (
+module tt_um_serv (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -22,33 +22,25 @@ module tt_um_spi_serv (
 
   // reg rst_n_i;
 
-  wire     spi_miso;
-  wire     spi_sck;
-  wire     spi_ss;
-  wire     spi_mosi;
   wire 	   q;
   wire 	   uart_txd;
 
-  assign uo_out[0] = spi_sck;
-  assign uo_out[1] = spi_ss;
-  assign uo_out[2] = spi_mosi;
-  assign uo_out[3] = q;
-  assign uo_out[4] = uart_txd;
+  assign uo_out[0] = q;
+  assign uo_out[1] = uart_txd;
   assign spi_miso = ui_in[0];
 
-  assign uo_out[7:5] = 3'h0;
+  assign uo_out[7:2] = 6'h0;
   assign uio_out = 8'h0;
   assign uio_oe = 8'h0;
 
    assign uart_txd = q;
 
-   servant_spi_top servant
+   servant
+     #(.memfile (memfile),
+       .memsize (memsize))
+   servant
      (.wb_clk (clk),
       .wb_rst (!rst_n),
-      .spi_miso(spi_miso),
-      .spi_sck(spi_sck),
-      .spi_ss(spi_ss),
-      .spi_mosi(spi_mosi),
       .q      (q));
 
 
